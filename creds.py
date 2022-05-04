73aa27b5958875d02a223f077f51b6a68014e63e
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2022-05-04 07:56:10 -0700

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,13 @@
+# CLASH OF CLANS API
+# Your Clash of Clash Developer Account email & password will be used to auto-generate a Key,
+# so don't worry about creating a new Key
+coc_dev_email = "11thunderstorm11@gmail.com"  # Clash of Clans Developer Account email
+coc_dev_password = "Iamapenguin69"  # Clash of Clans Developer Account password
+
+# DISCORD DEVELOPER
+discord_bot_token = "ur mom"  # 59 character Bot Token from your Discord App
+
+# OTHER INFORMATION
+clan_tag = "#2QRY8GP2Y"  # Your clan's tag - this doesn't have to be in creds, but if you're going to use it a lot...
+default_channel = 970443741942935562  # Default Discord Channel ID for messages
+war_channel = 970710259167535105  # Discord Channel ID for war reporting
